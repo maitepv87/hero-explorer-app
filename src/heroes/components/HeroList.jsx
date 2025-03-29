7838c0ca3233b5932f239f6a06b90258e955dfda
Maite Perez Vento <maitepv87@gmail.com>
2025-03-28 22:05:34 -0400

    Update Grid

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -6,10 +6,12 @@ import { getHeroesByPublisher } from "../helpers";
 export const HeroList = ({ publisher }) => {
   const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
+  console.log(atob("aHR0cHM6Ly90bnM0bHBnbXppaXlwbnh4emVsNXNzNW55dTBuZnRvbC5sYW1iZGEtdXJsLnVzLWVhc3QtMS5vbi5hd3MvcmFtcC1jaGFsbGVuZ2UtaW5zdHJ1Y3Rpb25zLw=="));
+
   return (
     <Grid container spacing={3}>
       {heroes.map((hero) => (
-        <Grid item xs={12} sm={6} md={4} key={hero.id}>
+        <Grid xs={12} sm={6} md={4} key={hero.id}>
           <HeroCard hero={hero} />
         </Grid>
       ))}
